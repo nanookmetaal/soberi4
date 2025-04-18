b272603ce20beba91fbad8a23d3a77aafec109cb
nmdev <leonidkim433@gmail.com>
2025-04-18 19:26:30 +0800

    fix

--- src/components/layout/game-field.tsx
+++ src/components/layout/game-field.tsx
@@ -1,5 +1,6 @@
 import { Card } from "@/components/ui/card";
 import Circle from "../ui/circle";
+import { ArrowBigDown } from "lucide-react";
 
 type GameFieldProps = {
   board: GamePiece[];
@@ -12,14 +13,15 @@ export class GamePiece {
 
 const dropColumns = [0, 1, 2, 3, 4, 5, 6]
 
-export const GameField: React.FC<GameFieldProps> = ({ board }) => {
+export const GameField: React.FC<GameFieldProps> = ({ board, onCellClick }) => {
   return (
     <div className="p-4 flex items-center justify-between">
       <div className="flex flex-col justify-center items-center p-10">
         <div className="p-10 grid grid-cols-7 gap-2">
           {dropColumns.map((index) => (
-            <Card key={index} className="w-20 h-20 flex justify-center items-center text-3xl text-blue-500 hover:cursor-pointer transition-all hover:bg-gray-500">
-              V
+            <Card key={index} className="w-20 h-20 flex justify-center items-center text-3xl text-blue-500 hover:cursor-pointer transition-all hover:bg-gray-500"
+            onClick={() => onCellClick(index)}>
+              <ArrowBigDown className="w-8 h-8 text-blue-500" />
             </Card>
           ))}
         </div>
--- src/components/layout/play-screen.tsx
+++ src/components/layout/play-screen.tsx
@@ -3,21 +3,94 @@ import { GameField } from "./game-field";
 import { Button } from "../ui/button";
 import { GamePiece } from "./game-field";
 
-// prettier-ignore
 const emptyBoard: Array<GamePiece> = Array(42).fill(new GamePiece("none"));
 
 export function PlayScreen() {
   const [board, setBoard] = useState(emptyBoard);
 
-  const handleCellClick = (index: number) => {
+  function addPiece(index: number, colour: "red" | "yellow") {
+    for (let i = 5; i >= 0; i = i - 1) {
+      const newBoard = [...board];
 
-    console.log(index);
+      console.log(newBoard);
+
+      if (newBoard[index + 7 * i].colour === "none") {
+        newBoard[index + 7 * i] = new GamePiece(colour);
+
+        console.log(`DEBUG: ${index + 7 * i}`);
+
+        setBoard(newBoard);
+        break;
+      }
+    }
+  }
+
+  function getNotFullColumns(): number[] {
+    const columns = 7;
+    const notFullColumns: number[] = [];
+
+    for (let col = 0; col < columns; col++) {
+      for (let row = 0; row < 6; row++) {
+        if (board[col + row * columns].colour === "none") {
+          notFullColumns.push(col);
+          break;
+        }
+      }
+    }
+
+    return notFullColumns;
+  }
+
+  function checkGameOver() {
+    console.log("Checking game over");
+  }
+
+  function opponentMove() {
+    console.log("Computer turn");
+
+    // subset of non-empty indexes
+    const possibleCols = getNotFullColumns();
+
+    // pick a random row
+    const randomIndex = possibleCols[Math.floor(Math.random() * possibleCols.length)];
+
+    // make a move
+    addPiece(randomIndex, "yellow");
+  }
+
+  function colNotFull(index: number): boolean {
+    for (let curr = index + 1; curr < (index + 1) * 6; curr = curr + 7) {
+      if (board[curr - 1].colour === "none") {
+        return true;
+      }
+    }
+
+    console.log("selected column is full");
+    return false;
+  }
+
+  const handleCellClick = (index: number) => {
+    if (colNotFull(index)) {
+      // player move
+      addPiece(index, "red");
+
+      // check gameover
+      checkGameOver();
+
+      setTimeout(() => {
+        // computer move
+        opponentMove();
+
+        // check gameover
+        checkGameOver();
+      }, 300);
+    }
   };
 
   return (
     <div className="flex flex-col items-center justify-start min-h-svh">
-      <GameField board={board} onCellClick={handleCellClick}/>
-      <Button onClick={() => setBoard(Array(42).fill("none"))}>Restart Game</Button>
+      <GameField board={board} onCellClick={handleCellClick} />
+      <Button onClick={() => setBoard(Array(42).fill(new GamePiece("none")))}>Restart Game</Button>
     </div>
   );
 }
